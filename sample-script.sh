d07dd98ace8a21d838a88562da0720b46dc8270b
Ubuntu <ubuntu@ip-172-31-26-28.ec2.internal>
2024-11-27 14:45:19 +0000

    my first commit

---
+++ sample-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# create an folder
+mkdir users
+# create two files inside the folder
+ cd users
+ touch downloads desktop
+
+# git sample script version system 
+#  follow the git stargedey 
+#
+
